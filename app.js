041f33558f194e29694b8df8ec5b82e5f957fbf2
Jumana14 <jumana5314@gmail.com>
2025-10-07 21:12:19 +0530

    Added a line of code in app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const http = require('http')
 const path = require('path');
 const cors = require('cors');
 const app = express();
@@ -23,3 +24,13 @@ app.use('/api/drama', dramaRouter)
 app.listen(port,()=>{
         console.log('Server is listening on port ',port);
 })
+
+//how to push code from your branch to main branch
+/*
+made changes in my branch and push in main branch
+1. git add .
+2. git commit -m ""
+3. git switch main
+4. git pull origin main
+5. git merge branch_name
+*/
