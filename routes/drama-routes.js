1922518141e003a3763d5919b379f85b8ebabb64
Jumana14 <jumana5314@gmail.com>
2025-10-07 21:08:37 +0530

    Updated drama-routes file

--- routes/drama-routes.js
+++ routes/drama-routes.js
@@ -4,12 +4,19 @@ const {insertOneDrama, getAllDramas, insertManyDrama, getDramaById, updateDramaU
 
 const router = express.Router();
 
+//to get all the drama list 
 router.get('/',getAllDramas);
+// to create one drama
 router.post('/one',insertOneDrama);
+//to create many drama
 router.post('/many',insertManyDrama);
+//to get particular drama using id
 router.get('/:id',getDramaById);
+//to update particular drama using id
 router.put('/:id',updateById);
+//to delete particular drama using id
 router.delete('/:id',deleteById);
+
 router.put('/:id',updateDramaUsingPut)
 
 module.exports(router);
